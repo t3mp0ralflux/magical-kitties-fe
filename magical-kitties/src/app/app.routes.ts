ca8fe0cfd241382fb681d2f9f408ed0ad64e0983
Brent <brenton.belanger@gmail.com>
2025-05-15 10:22:28 -0400

    Initial commit!

---
+++ magical-kitties/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+
+export const routes: Routes = [
+    { path: '', component: LandingComponent, pathMatch: 'full' }
+];
