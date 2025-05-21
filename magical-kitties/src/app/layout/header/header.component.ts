38d7eff39bbd8c8118556ed9682e96bfc44ea54b
Brent <brenton.belanger@gmail.com>
2025-05-21 11:33:35 -0400

    Removed menu as it really doesn't quite fit right now.

--- magical-kitties/src/app/layout/header/header.component.ts
+++ magical-kitties/src/app/layout/header/header.component.ts
@@ -1,14 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-header',
-    imports: [MatToolbar, MatToolbarRow, MatDivider, MatButton],
+    imports: [CommonModule, MatToolbar, MatToolbarRow, MatDivider, MatButton, MatMenu, MatMenuTrigger, MatCard, MatCardContent, MatIcon],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+    showCharacters = false;
+    fieldShown = "";
 }
