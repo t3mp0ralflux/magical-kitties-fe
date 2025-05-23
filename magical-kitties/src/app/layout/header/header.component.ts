ee62e8f95edec73edcb79d50bd12cc7cbd33d7f1
Brent <brenton.belanger@gmail.com>
2025-05-23 14:36:49 -0400

    aww yiss

--- magical-kitties/src/app/layout/header/header.component.ts
+++ magical-kitties/src/app/layout/header/header.component.ts
@@ -1,15 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
-import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
-import { MatIcon } from '@angular/material/icon';
-import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-header',
-    imports: [CommonModule, MatToolbar, MatToolbarRow, MatDivider, MatButton, MatMenu, MatMenuTrigger, MatCard, MatCardContent, MatIcon],
+    imports: [CommonModule, MatToolbar, MatToolbarRow, MatDivider, MatButton],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
