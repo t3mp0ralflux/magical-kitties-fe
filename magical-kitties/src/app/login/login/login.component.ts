8d2d5ff0dc173c7fd444c7ebf700ecfdc1cead05
Brent <brenton.belanger@gmail.com>
2025-06-10 15:20:21 -0400

    better login stuff

--- magical-kitties/src/app/login/login/login.component.ts
+++ magical-kitties/src/app/login/login/login.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LoginModel } from '../../models/login.model';
 import { AuthService } from '../../services/authService.service';
@@ -13,7 +15,7 @@ import { GoogleComponent } from "../google/google.component";
 
 @Component({
     selector: 'app-login',
-    imports: [CommonModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatInputModule, RouterLink, RouterLinkActive, MatProgressSpinnerModule, MatIconModule, GoogleComponent],
+    imports: [CommonModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatInputModule, RouterLink, RouterLinkActive, MatProgressSpinnerModule, MatIconModule, GoogleComponent, MatDividerModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss'
 })
@@ -23,6 +25,8 @@ export class LoginComponent {
     loginError: boolean = false;
     hidePassword: boolean = true;
 
+    private _snackBar = inject(MatSnackBar);
+
     constructor(
         private fb: FormBuilder,
         private authSerivce: AuthService,
@@ -30,6 +34,16 @@ export class LoginComponent {
         this.formGroup = fb.group({
             email: new FormControl(null, [Validators.required]),
             password: new FormControl(null, [Validators.required])
+        })
+    }
+
+    openSnackBar(message: string, action: string) {
+        let snackBar: MatSnackBarRef<TextOnlySnackBar> = this._snackBar.open(message, action, {
+        });
+        snackBar.onAction().subscribe({
+            next: (thing: any) => {
+                this.login();
+            }
         })
     }
 
@@ -53,9 +67,10 @@ export class LoginComponent {
                         break;
 
                     case 500:
-                        alert("WHAT DID YOU DO?!");
+                        this.openSnackBar("Internal Server Error", "Retry");
                         break;
                     default:
+                        this.openSnackBar("Something went wrong.", "Retry");
                         break;
                 }
                 this.formSubmitting = false;
